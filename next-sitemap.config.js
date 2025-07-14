2aa24013eb6f18a51f0a0fc0019fff36c6b3ff3e
Jason Rodriguez <jasontylerrodriguez@gmail.com>
2025-07-13 19:31:43 -0700

    Adds usage of next-sitemap for automated sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.datadustr.com',
+  generateRobotsTxt: true,
+  changefreq: 'monthly',
+  sitemapSize: 5000,
+  exclude: [''],
+};
